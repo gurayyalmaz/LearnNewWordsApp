5c2fb4db0d559a3223848a42fd7829dc3eb9d665
gurayyalmaz <guray.yl93@gmail.com>
2023-10-23 02:35:58 +0200

    created word model to model file and created word model entity to core data model

---
+++ LearnNewWordsApp/Model/WordModel.swift
@@ -0,0 +1,14 @@
+//
+//  WordModel.swift
+//  LearnNewWordsApp
+//
+//  Created by Gyuray Yalmaz on 23.10.23.
+//
+
+import Foundation
+
+struct Word {
+    let inForeignLanguage: String
+    let meaning: String
+    let id: UUID
+}
